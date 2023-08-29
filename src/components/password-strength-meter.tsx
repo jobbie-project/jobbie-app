cc83a434ec35b934b3c10ca2532055339e2124a0
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2023-08-29 16:37:34 -0300

    refactor and create .prettier.rx.js

--- src/components/password-strength-meter.tsx
+++ src/components/password-strength-meter.tsx
@@ -5,69 +5,37 @@ interface PasswordStrengthMeterProps {
   password: string;
 }
 
-const PasswordStrengthMeter: React.FC<PasswordStrengthMeterProps> = ({
-  password,
-}) => {
+const PasswordStrengthMeter: React.FC<PasswordStrengthMeterProps> = ({ password }) => {
   const testResult = zxcvbn(password);
   const createPassLabel = () => {
-    switch (testResult.score) {
-      case 0:
-        return "Muito fraco";
-      case 1:
-        return "Fraco";
-      case 2:
-        return "Bom";
-      case 3:
-        return "Forte";
-      case 4:
-        return "Excelente";
-      default:
-        return "";
-    }
+    const textScore = ["Muito fraco", "Fraco", "Bom", "Forte", "Excelente"];
+    return textScore[testResult.score];
   };
 
   const funcProgressColor = () => {
-    switch (testResult.score) {
-      case 0:
-        return "#f1f1f1";
-      case 1:
-        return "#E30000";
-      case 2:
-        return "#FFD80D";
-      case 3:
-        return "#51BC65";
-      case 4:
-        return "#229337";
-      default:
-        return "#F1F1F1";
-    }
+    const colorsScore = ["#f1f1f1", "#E30000", "#FFD80D", "#51BC65", "#229337"];
+    return colorsScore[testResult.score];
   };
 
   return (
-    <div
-      className="rounded-lg mt-4 flex flex-row items-center justify-between"
-      style={{ height: "6px" }}
-    >
+    <div className="rounded-lg mt-4 flex flex-row items-center justify-between" style={{ height: "6px" }}>
       <div className="w-full flex flex-row">
         {Array(4)
           .fill(0)
           .map((_, index) => (
             <div
+              key={index}
               style={{
                 width: "25%",
                 height: "6px",
-                background:
-                  index < testResult.score ? funcProgressColor() : "#F1F1F1",
+                background: index < testResult.score ? funcProgressColor() : "#F1F1F1",
               }}
               className={`rounded-lg ${index !== 0 && "ml-2"}`}
             />
           ))}
       </div>
 
-      <span
-        className="text-sm min-w-[80px] text-end ml-4"
-        style={{ textDecorationColor: funcProgressColor() }}
-      >
+      <span className="text-sm min-w-[80px] text-end ml-4" style={{ textDecorationColor: funcProgressColor() }}>
         {createPassLabel()}
       </span>
     </div>
--- src/components/route-button.tsx
+++ src/components/route-button.tsx
@@ -4,15 +4,13 @@ interface RouteButtonProps {
   text: string;
   link: string;
   type: "button" | "submit" | "reset";
+  className?: string;
 }
 
 export function RouteButton(props: RouteButtonProps) {
   return (
     <Link className="w-full flex justify-center" to={props.link}>
-      <button
-        type={props.type}
-        className="mt-6 max-w-sm px-6 text-lg bg-red font-normal w-full text-white rounded py-3 block shadow-xl"
-      >
+      <button type={props.type} className={`mt-6 max-w-sm px-6 text-lg bg-red font-normal w-full text-white rounded py-3 block shadow-xl ${props.className}`}>
         {props.text}
       </button>
     </Link>
