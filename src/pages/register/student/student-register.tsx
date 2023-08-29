ec41c53f20d9bceab955c2817897baa03a8c7b5a
Adrielly Isly <114019643+islycodes@users.noreply.github.com>
2023-08-29 16:36:29 -0300

    create verifyemail

--- src/pages/register/student/student-register.tsx
+++ src/pages/register/student/student-register.tsx
@@ -7,18 +7,25 @@ import { useNavigate } from "react-router-dom";
 
 export default function StudentRegister() {
   const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
   const onSubmit = () => {
     navigate("/verifica-email");
   };
+
   return (
     <div>
       <RegisterHeader />
       <form onSubmit={onSubmit}>
         <div className="max-w-full items-center p-5 flex flex-col min-h-screen mt-6">
           <div className="max-w-xs w-full">
-            <p className="text-black font-semibold text-xl mt-20">Criar uma conta</p>
-            <p className="mt-6">Preencha com as informações que deseja utilizar para realizar login no portal.</p>
+            <p className="text-black font-semibold text-xl mt-20">
+              Criar uma conta
+            </p>
+            <p className="mt-6">
+              Preencha com as informações que deseja utilizar para realizar
+              login no portal.
+            </p>
           </div>
           <form action="" className="w-full max-w-[336px]">
             <div className="mt-8 w-full">
@@ -45,7 +52,11 @@ export default function StudentRegister() {
             </div>
             <div className="mt-2">
               {" "}
-              <RouteButton type="submit" text="Continuar" link={`/verifica-email`} />
+              <RouteButton
+                type="submit"
+                text="Continuar"
+                link={`/verifica-email`}
+              />
             </div>
           </form>
         </div>
--- src/pages/register/student/verify-email.tsx
+++ src/pages/register/student/verify-email.tsx
@@ -1,8 +1,28 @@
-import PasswordStrengthMeter from "@/components/password-strength-meter";
-import { useState } from "react";
+import RegisterHeader from "@/components/register-header";
+import { useNavigate } from "react-router-dom";
 
 export default function VerifyEmail() {
-  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const onSubmit = () => {
+    navigate("/");
+  };
 
-  return <>);</>;
+  return (
+    <div>
+      <RegisterHeader />
+      <form onSubmit={onSubmit}>
+        <div className="max-w-full items-center p-5 flex flex-col min-h-screen mt-6">
+          <div className="max-w-xs w-full">
+            <p className="text-black font-semibold text-xl mt-20">
+              Verifique sua conta para continuar
+            </p>
+            <p className="mt-6">
+              Um link de ativação foi enviado para o endereço de email que você
+              forneceu.
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
