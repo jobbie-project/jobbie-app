189fcb0827ccfa99e94ad0a3881c9131961d6c67
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2023-09-04 13:27:30 -0300

    JOB-8 implements and set react-redux
    also inserts a file to store the state and cities of Brazil
    
    Co-Authored-By: Adrielly Isly <114019643+islycodes@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import {App} from './App.tsx';
 import {BrowserRouter} from 'react-router-dom';
 import {ThemeProvider} from '@material-tailwind/react';
-
+import {Provider} from 'react-redux';
+import store from './store/store.ts';
 import 'react-toastify/dist/ReactToastify.css';
 import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/store/slices/profile-data.ts
@@ -0,0 +1,120 @@
+import {EducationLevel} from '@/interfaces/education-level';
+import {createSlice} from '@reduxjs/toolkit';
+
+export const profileDataSlice = createSlice({
+  name: 'profileData',
+  initialState: {
+    name: '',
+    phone: '',
+    address: {
+      street: '',
+      city: '',
+      state: '',
+      zip_code: '',
+    },
+    education: [
+      {
+        institution_name: '',
+        degree: '',
+        level: null as EducationLevel | null,
+        start_date: '',
+        end_date: '',
+        actual_cycle: '',
+        location: {
+          city: '',
+          state: '',
+        },
+      },
+    ],
+    previous_experience: [
+      {
+        company_name: '',
+        position: '',
+        start_date: '',
+        end_date: '',
+        location: {
+          city: '',
+          state: '',
+        },
+        description: '',
+      },
+    ],
+    tech_stacks: [''],
+    certifications: [''],
+    links: {
+      github: '',
+      linkedin: '',
+      portfolio: '',
+    },
+  },
+  reducers: {
+    setUserAddress: (state, action) => {
+      state.address = action.payload;
+    },
+
+    setUserEducation: (state, action) => {
+      state.education.push(action.payload);
+    },
+    updateUserEducation: (state, action) => {
+      state.education[action.payload.index] = action.payload.education;
+    },
+    deleteUserEducation: (state, action) => {
+      state.education.splice(action.payload.index, 1);
+    },
+
+    setUserPreviousExperience: (state, action) => {
+      state.previous_experience.push(action.payload);
+    },
+    updateUserPreviousExperience: (state, action) => {
+      state.previous_experience[action.payload.index] = action.payload.previousExperience;
+    },
+    deleteUserPreviousExperience: (state, action) => {
+      state.previous_experience.splice(action.payload.index, 1);
+    },
+
+    setUserLinks: (state, action) => {
+      state.links = action.payload;
+    },
+
+    setUserName: (state, action) => {
+      state.name = action.payload;
+    },
+
+    setUserPhone: (state, action) => {
+      state.phone = action.payload;
+    },
+
+    setUserTechStacks: (state, action) => {
+      state.tech_stacks.push(action.payload);
+    },
+    deleteUserTechStacks: (state, action) => {
+      state.tech_stacks.splice(action.payload.index, 1);
+    },
+
+    setUserCertifications: (state, action) => {
+      state.certifications.push(action.payload);
+    },
+    deleteUserCertifications: (state, action) => {
+      state.certifications.splice(action.payload.index, 1);
+    },
+  },
+});
+
+export const {
+  setUserAddress,
+  setUserEducation,
+  updateUserEducation,
+  deleteUserEducation,
+  setUserPreviousExperience,
+  updateUserPreviousExperience,
+  deleteUserPreviousExperience,
+  setUserLinks,
+  setUserName,
+  setUserPhone,
+  setUserTechStacks,
+  deleteUserTechStacks,
+  setUserCertifications,
+  deleteUserCertifications,
+} = profileDataSlice.actions;
+
+export default profileDataSlice.reducer;
